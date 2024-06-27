71d8ad7ae7d2633170bb4ca7798a78b594b78482
shivam9333 <149943462+shivam9333@users.noreply.github.com>
2024-06-27 09:39:54 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -64,3 +64,9 @@ function custom_search_script() {
     }
 }
 add_action('wp_footer', 'custom_search_script');
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url(home_url('/')); ?>">
+    <div>
+        <label class="screen-reader-text" for="s"><?php echo _x('Search for:', 'label'); ?></label>
+        <input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+    </div>
+</form>
