8d1b091113872f225a0b7dee68350649eba9c814
shivam9333 <149943462+shivam9333@users.noreply.github.com>
2024-07-02 12:37:24 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -127,3 +127,67 @@ function get_country_from_ip($ip) {
     }
 }
 
+class Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
+
+    // Start Level
+    function start_lvl( &$output, $depth = 0, $args = array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"dropdown-menu\">\n";
+    }
+
+    // Start Element
+    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+        $indent = ($depth) ? str_repeat("\t", $depth) : '';
+
+        // Add 'nav-item' class to <li>
+        $classes = empty($item->classes) ? array() : (array) $item->classes;
+        $classes[] = 'nav-item';
+
+        // Check if item has children
+        if (in_array('menu-item-has-children', $classes)) {
+            $classes[] = 'dropdown';
+        }
+
+        // Join the classes
+        $class_names = join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args));
+        $class_names = $class_names ? ' class="' . esc_attr($class_names) . '"' : '';
+
+        // Start the <li> element
+        $output .= $indent . '<li' . $class_names . '>';
+
+        // Link attributes
+        $atts = array();
+        $atts['title']  = !empty($item->attr_title) ? $item->attr_title : '';
+        $atts['target'] = !empty($item->target) ? $item->target : '';
+        $atts['rel']    = !empty($item->xfn) ? $item->xfn : '';
+        $atts['href']   = !empty($item->url) ? $item->url : '';
+
+        // Add 'nav-link' class to <a>
+        $atts['class'] = 'nav-link';
+        if (in_array('menu-item-has-children', $classes)) {
+            $atts['class'] .= ' dropdown-toggle';
+            $atts['data-toggle'] = 'dropdown';
+        }
+
+        // Prepare the link attributes
+        $atts = apply_filters('nav_menu_link_attributes', $atts, $item, $args);
+        $attributes = '';
+        foreach ($atts as $attr => $value) {
+            if (!empty($value)) {
+                $value = ('href' === $attr) ? esc_url($value) : esc_attr($value);
+                $attributes .= ' ' . $attr . '="' . $value . '"';
+            }
+        }
+
+        // Build the item output
+        $item_output = $args->before;
+        $item_output .= '<a' . $attributes . '>';
+        $item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID) . $args->link_after;
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+        // Finalize the output
+        $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+    }
+}
+
